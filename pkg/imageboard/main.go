34f30a8d738d6b5fb839e3eb14a2217ceb499451
toodemhard <lailokkay@gmail.com>
2023-09-23 16:21:01 +0100

    Add create thread endpoint

--- pkg/imageboard/main.go
+++ pkg/imageboard/main.go
@@ -1,18 +1,33 @@
 package imageboard
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 )
 
+func postThread(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		thread := Thread{}
+		if err := c.Bind(&thread); err != nil {
+			return err
+		}
+		fmt.Println(thread)
+		CreateThread(db, thread)
+		return c.HTML(http.StatusOK, `<div>done it m8<\div>`)
+
+	}
+}
+
 func Run() {
 	db := ConnectToDB()
-	_ = db
 
 	e := echo.New()
 	e.Static("/", "static")
+	e.POST("/thread", postThread(db))
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
