fe0dc28a55f6fdf9a5f7b531bdf4d332ec710c14
toodemhard <lailokkay@gmail.com>
2023-09-25 12:37:59 +0100

    Render page of threads + new thread form

--- pkg/imageboard/main.go
+++ pkg/imageboard/main.go
@@ -1,7 +1,8 @@
 package imageboard
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 
@@ -9,25 +10,50 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const publicDir = "../../public/"
+
 func postThread(db *sqlx.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		thread := Thread{}
 		if err := c.Bind(&thread); err != nil {
 			return err
 		}
-		fmt.Println(thread)
 		CreateThread(db, thread)
-		return c.HTML(http.StatusOK, `<div>done it m8<\div>`)
+		return c.HTML(http.StatusOK, `<div>done it m8`)
+	}
+}
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func getThreads(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		threads, err := GetAllThreads(db)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "db query failed")
+		}
+		return c.Render(http.StatusOK, "thread.html", threads)
 	}
 }
 
 func Run() {
 	db := ConnectToDB()
-
 	e := echo.New()
-	e.Static("/", "static")
+
+	t := &Template{
+		template.Must(template.ParseFiles(publicDir + "thread.html")),
+	}
+
+	e.Renderer = t
+
+	e.Static("/assets", publicDir+"assets")
 	e.POST("/thread", postThread(db))
+	e.GET("/", getThreads(db))
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
