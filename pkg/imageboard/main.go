dbdd49b41e94adbbe4f0d2a8602580aa4311519b
toodemhard <lailokkay@gmail.com>
2023-09-26 12:07:38 +0100

    Use handler methods Reduces nesting.

--- pkg/imageboard/main.go
+++ pkg/imageboard/main.go
@@ -13,44 +13,40 @@ import (
 
 const publicDir = "../../public/"
 
-func postThread(db *sqlx.DB) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		thread := Thread{}
-		if err := c.Bind(&thread); err != nil {
-			return err
-		}
-		CreateThread(db, thread)
-		return c.HTML(http.StatusOK, `<div>done it m8`)
+func (h *Handler) postThread(c echo.Context) error {
+	thread := Thread{}
+	if err := c.Bind(&thread); err != nil {
+		return err
 	}
+	CreateThread(h.db, thread)
+	return c.HTML(http.StatusOK, `<div>done it m8`)
 }
 
-func getThread(db *sqlx.DB) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		id, err := strconv.Atoi(c.Param("id"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "invalid thread id")
-		}
-
-		thread, err := queryThread(db, int64(id))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "db query failed")
-		}
-
-		replies, err := queryThreadReplies(db, id)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusNotFound, "db query failed")
-		}
-
-		page := struct {
-			Thread  Thread
-			Replies []Reply
-		}{
-			thread,
-			replies,
-		}
-
-		return c.Render(http.StatusOK, "thread.html", page)
+func (h *Handler) getThread(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "invalid thread id")
 	}
+
+	thread, err := queryThread(h.db, int64(id))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "db query failed")
+	}
+
+	replies, err := queryThreadReplies(h.db, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "db query failed")
+	}
+
+	page := struct {
+		Thread  Thread
+		Replies []Reply
+	}{
+		thread,
+		replies,
+	}
+
+	return c.Render(http.StatusOK, "thread.html", page)
 }
 
 type Template struct {
@@ -65,30 +61,33 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return err
 }
 
-func index(db *sqlx.DB) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		threads, err := queryAllThreads(db)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "db query failed")
-		}
-		return c.Render(http.StatusOK, "index.html", threads)
+func (h *Handler) index(c echo.Context) error {
+	threads, err := queryAllThreads(h.db)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "db query failed")
 	}
+	return c.Render(http.StatusOK, "index.html", threads)
+}
+
+type Handler struct {
+	db *sqlx.DB
 }
 
 func Run() {
 	db := ConnectToDB()
 	e := echo.New()
 
-	t := &Template{
+	e.Renderer = &Template{
 		template.Must(template.ParseGlob(publicDir + "*.html")),
 	}
 
-	e.Renderer = t
+	h := Handler{db}
 
 	e.Static("/assets", publicDir+"assets")
-	e.POST("/thread", postThread(db))
-	e.GET("/thread/:id", getThread(db))
-	e.GET("/", index(db))
+	e.POST("/thread", h.postThread)
+	e.GET("/thread/:id", h.getThread)
+	// e.POST("/thread/:id/reply", getThread(db))
+	e.GET("/", h.index)
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
