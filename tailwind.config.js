c89b4c6e2e7ea6340ebdc4e06c64321562893e2a
toodemhard <lailokkay@gmail.com>
2023-10-03 12:09:05 +0100

    Restructure templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/*.{html,js}"],
+    content: ["./templates/src/**/*.{html,js}"],
     theme: {
         extend: {},
     },
